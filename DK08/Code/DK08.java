28777ee9ba91eecbc5da40870a960e5fc75d3ee0
hoangnv725 <hoangnv725@gmail.com>
2025-01-11 09:28:37 +0700

    update

---
+++ DK08/Code/DK08.java
@@ -0,0 +1,24 @@
+package Code;
+
+import java.util.Scanner;
+
+public class DK08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		char op = sc.next().charAt(0);
+		double b = sc.nextDouble();
+		
+		if (op == '+') System.out.printf("%.2f", a + b);
+		else if (op == '-') System.out.printf("%.2f", a - b);
+		else if (op == '*') System.out.printf("%.2f", a * b);
+		else if (op == '/') {
+			if (b != 0) System.out.printf("%.2f", a / b);
+			else System.out.print("Math Error");
+		}
+		
+		sc.close();
+	}
+
+}
